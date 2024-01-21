82220512be6cc2a97efcb808ec900a220ae1b26e
bokob <bmc0520@naver.com>
2024-01-21 14:57:15 +0900

    Raycast1

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -26,6 +26,13 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position + Vector3.up, Vector3.forward, out hit, 10))
+        {
+            Debug.Log($"Raycast {hit.collider.gameObject.name}!");
+        }
     }
 }
