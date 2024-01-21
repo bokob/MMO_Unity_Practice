af9cff60ebd211dd1b5ba8cdacdd7f6e66d0b4da
bokob <bmc0520@naver.com>
2024-01-21 16:18:00 +0900

    Raycast2

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -26,13 +26,39 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        // Local <-> World <-> ViewPort <-> Screen(È­¸é)
 
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position + Vector3.up, Vector3.forward, out hit, 10))
+        //Debug.Log(Input.mousePosition); // Screen
+
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // ViewPort
+
+        if (Input.GetMouseButton(0))
         {
-            Debug.Log($"Raycast {hit.collider.gameObject.name}!");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition );
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
         }
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 dir = mousePos - Camera.main.transform.position;
+        //    dir = dir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
+        //    {
+        //        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //    }
+        //}
+
     }
 }
