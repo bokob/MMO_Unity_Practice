3678b7f28ad3b5da4019966a330e6135263f4b1b
bokob <bmc0520@naver.com>
2024-01-24 01:19:39 +0900

    Coroutine

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    Coroutine co;
 	protected override void Init()
     {
         base.Init();
@@ -12,9 +14,31 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-		for (int i = 0; i < 5; i++)
-			Managers.Resource.Instantiate("UnityChan");
-	}
+        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
+        StartCoroutine("CoStopExplode", 2.0f);
+	    
+        StopCoroutine(co);
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Excute!!!");
+        if(co!=null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Excute!!!");
+        co = null;
+    }
 
     public override void Clear()
     {
