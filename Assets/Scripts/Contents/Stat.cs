1a4fc584750181c3d97e56d80d5b39a627c2d276
bokob <bmc0520@naver.com>
2024-01-28 17:04:05 +0900

    스탯

---
+++ Assets/Scripts/Contents/Stat.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat : MonoBehaviour
+{
+    [SerializeField]
+    protected int _level;
+    [SerializeField]
+    protected int _hp;
+    [SerializeField]
+    protected int _maxHp;
+    [SerializeField]
+    protected int _attack;
+    [SerializeField]
+    protected int _defense;
+    [SerializeField]
+    protected float _moveSpeed;
+
+    public int Level { get { return _level; } set { _level = value; } }
+    public int Hp { get { return _hp; } set { _hp = value; } }
+    public int MaxHp { get { return _maxHp; } set { _maxHp = value; } }
+    public int Attack { get { return _attack; } set { _attack = value; } }
+    public int Defence { get { return _defense; } set { _defense = value; } }
+    public float MoveSpeed { get { return _moveSpeed; } set { _moveSpeed = value; } }
+
+    private void Start()
+    {
+        _level = 0;
+        _hp = 0;
+        _maxHp = 0;
+        _attack = 0;
+        _defense = 5;
+        _moveSpeed = 5.0f;
+    }
+}
