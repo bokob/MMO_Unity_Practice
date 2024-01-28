54b8e43e3400e037285e272845371bfa376bbb69
bokob <bmc0520@naver.com>
2024-01-28 17:51:51 +0900

    마우스 커서

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,12 +6,25 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
 	PlayerStat _stat;
-
 	Vector3 _destPos;
+
+	Texture2D _attackIcon;
+	Texture2D _handIcon;
+
+	enum CursorType
+	{
+		None,
+		Attack,
+		Hand,
+	}
+	CursorType _cursorType = CursorType.None;
 
     void Start()
     {
-		_stat = GetComponent<PlayerStat>();
+        _attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
+
+        _stat = GetComponent<PlayerStat>();
 
 		Managers.Input.MouseAction -= OnMouseClicked;
 		Managers.Input.MouseAction += OnMouseClicked;		
@@ -75,7 +88,9 @@ public class PlayerController : MonoBehaviour
 
 	void Update()
     {
-		switch (_state)
+		UpdateMouseCursor();
+
+        switch (_state)
 		{
 			case PlayerState.Die:
 				UpdateDie();
@@ -88,6 +103,34 @@ public class PlayerController : MonoBehaviour
 				break;
 		}
 	}
+
+	void UpdateMouseCursor()
+	{
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, _mask))
+        {
+
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+				if(_cursorType != CursorType.Attack)
+				{
+                    Cursor.SetCursor(_attackIcon, new Vector2(_attackIcon.width / 5, 0), CursorMode.Auto);
+					_cursorType = CursorType.Attack;
+                }
+			}
+            else
+            {
+
+                if (_cursorType != CursorType.Hand)
+                {
+                    Cursor.SetCursor(_handIcon, new Vector2(_handIcon.width / 3, 0), CursorMode.Auto);
+                    _cursorType = CursorType.Hand;
+                }
+			}
+		}
+    }
 
 	int _mask = (1 << (int)Define.Layer.Ground | (1 << (int)Define.Layer.Monster));
 	void OnMouseClicked(Define.MouseEvent evt)
